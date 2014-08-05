5588191034cb779d2f082073aa0b9cf521f0420f
germanf <ferreyra.ger@gmail.com>
2014-08-05 08:05:19 -0300

    + Min, Avg and Max functions + styles + Min, Avg and Max bindings

--- webapp/app/scripts/viewmodels/chart-vm.js
+++ webapp/app/scripts/viewmodels/chart-vm.js
@@ -18,22 +18,33 @@ function ChartViewModel(webApiSvc, $timeout) {
   self.getDataForDate = getDataForDate;
 
   self.chartData = getChartData;
+  self.isLoadingCharts = false;
 
-  // privates
+  self.setDefaultData = setDefaultData;
+  self.setMinData = setMinData;
+  self.setAvgData = setAvgData;
+  self.setMaxData = setMaxData;
+
+  // functions
   function getChartData() {
-    //transform charts
+    var chartData = { series: [], data: [] };
+    for (var chart in self.model.charts) {
+      if (self.isChartLoaded(chart)) {
+        chartData.series.push(chart.description);
+        for (var data in chart.data) {
+        }
+      }
+    }
+    return chartData;
+  }
+
+  function isChartLoaded(chart) {
+    return self.model.charts.hasOwnProperty(chart) && chart.data && chart.data.length;
   }
 
   function initModel() {
     return {
       charts: [],
-      //currentChart: {
-      //  id: '',
-      //  name: '',
-      //  description: '',
-      //  data: []
-      //},
-
       dataForDate: {
         date: '',
         data: []
@@ -44,7 +55,6 @@ function ChartViewModel(webApiSvc, $timeout) {
   function initConfig() {
     return {
       labels: false,
-      title: "Percona test chart",
       legend: {
         display: true,
         htmlEnabled: true,
@@ -55,11 +65,13 @@ function ChartViewModel(webApiSvc, $timeout) {
   }
 
   function getCharts() {
-    self.webApiSvc.query()
+    return self.webApiSvc.query()
       .$promise.then(function (charts) {
         self.model.charts.length = 0;
         for (var i = 0; i < charts.length; i++) {
-          var chart = charts[i]
+          var chart = charts[i];
+          chart.chartType = 'bar';
+          chart.originalData = [];
           self.model.charts.push(chart);
           self.getChart(chart.id);
           self.getChartData(chart.id);
@@ -69,7 +81,7 @@ function ChartViewModel(webApiSvc, $timeout) {
 
   function getChart(chartId) {
     var id = chartId;
-    self.webApiSvc.get({ id: id })
+    return self.webApiSvc.get({ id: id })
       .$promise.then(function (chart) {
         self.model.charts.some(function (obj) {
           if (obj.id === id) {
@@ -81,13 +93,16 @@ function ChartViewModel(webApiSvc, $timeout) {
   };
 
   function getChartData(chartId) {
-    self.webApiSvc.getData({ id: chartId })
+    return self.webApiSvc.getData({ id: chartId })
       .$promise.then(function (result) {
         self.model.charts.some(function (chart) {
           if (chart.id === chartId) {
-            chart.data = [];
+            chart.data = { data: [], series: [] };
             for (var i = 0; i < result.length; i++) {
-              chart.data.push(result[i]);
+              var chartData = result[i];
+              chart.originalData.push(angular.copy(chartData));
+              chart.data.series.push(chartData.date);
+              chart.data.data.push({ x: chartData.date, y: chartData.data });
             }
             return true;
           }
@@ -96,16 +111,84 @@ function ChartViewModel(webApiSvc, $timeout) {
   };
 
   function getDataForDate(chartId, date) {
-    self.webApiSvc.get({ id: chartId, date: date })
+    return self.webApiSvc.get({ id: chartId, date: date })
       .$promise.then(function (result) {
         self.model.dataForDate.date = result.date;
 
         self.model.dataForDate.data.length = 0;
         for (var data in result) {
-          self.model.dataForDate.data.push(data);
+          var chartData = { x: data.date, y: data.data };
+          self.model.dataForDate.data.push(chartData);
         }
       });
   };
+
+  function setDefaultData(chart) {
+    var lenData = chart.data.data.length;
+    var data = chart.data;
+
+    for (var i = 0; i < lenData; i++) {
+      chart.data.data[i].y.length = 0;
+      chart.data.data[i].y = angular.copy(chart.originalData[i].data);
+    }
+  }
+
+  function setMinData(chart) {
+    var chartData = chart.data;
+    var originalData = angular.copy(chart.originalData);
+    var lenData = originalData.length;
+
+    for (var i = 0; i < lenData; i++) {
+      var min = Number.MAX_VALUE;
+      var originalY = originalData[i].data;
+      var yLength = originalY.length;
+      var y = chartData.data[i].y;
+
+      for (var j = 0; j < yLength; j++)
+        if (originalY[j] < min) min = originalY[j];
+
+      y.length = 0;
+      y.push(min);
+    }
+  }
+
+  function setAvgData(chart) {
+    var chartData = chart.data;
+    var originalData = angular.copy(chart.originalData);
+    var lenData = originalData.length;
+
+    for (var i = 0; i < lenData; i++) {
+      var sum = 0;
+      var originalY = originalData[i].data;
+      var yLength = originalY.length;
+      var y = chartData.data[i].y;
+
+      for (var j = 0; j < yLength; j++)
+        sum += originalY[j];
+
+      y.length = 0;
+      y.push(sum / yLength);
+    }
+  }
+
+  function setMaxData(chart) {
+    var chartData = chart.data;
+    var originalData = angular.copy(chart.originalData);
+    var lenData = originalData.length;
+
+    for (var i = 0; i < lenData; i++) {
+      var max = Number.MIN_VALUE;
+      var originalY = originalData[i].data;
+      var yLength = originalY.length;
+      var y = chartData.data[i].y;
+
+      for (var j = 0; j < yLength; j++)
+        if (originalY[j] > max) max = originalY[j];
+
+      y.length = 0;
+      y.push(max);
+    }
+  }
 
   return self;
 }
