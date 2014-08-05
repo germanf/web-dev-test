6355ecdc13d641c8b41e06fa7c8f666a0e742d87
germanf <ferreyra.ger@gmail.com>
2014-08-05 04:39:59 -0300

    + enabled cors

--- chart-api/app/server.js
+++ chart-api/app/server.js
@@ -1,8 +1,11 @@
-var express = require('express'), 
-    charts = require('./routes/charts.js');
+var express = require('express'),
+    charts = require('./routes/charts.js'),
+    cors = require('cors');
 
 var app = express();
- 
+
+app.use(cors());
+
 app.get('/charts', charts.findAll);
 app.get('/charts/:id', charts.findById);
 app.get('/charts/:id/data', charts.getChartData);
